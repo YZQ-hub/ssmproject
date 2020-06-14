41d074ed2adedfe6ef586c36ad0e523a44014d33
YZQ <2959837640@qq.com>
2020-06-14 16:06:09 +0800

    第三次提交

--- springboot/src/main/java/com/langren/model/User.java
+++ springboot/src/main/java/com/langren/model/User.java
@@ -1,9 +1,12 @@
 package com.langren.model;
 
 import java.io.Serializable;
+import java.util.Date;
 
 public class User implements Serializable{
     private Integer id;
     private String username;
     private Integer password;
+    private Date birthday;
+    private String email;
 }
